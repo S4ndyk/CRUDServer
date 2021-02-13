be10aea30eec5c0ef5537cd1ba89c839df509258
Santtu Lempinen <santtu@localhost.localdomain>
2021-02-13 16:31:11 +0200

    Add hello world message to root

--- index.js
+++ index.js
@@ -2,4 +2,9 @@ const express = require('express');
 const app = express();
 const PORT = 5000
 
+
+app.get('/', (req, res) => {
+    res.send('Hello from kubernetes')
+})
+
 app.listen(PORT, () => console.log(`Server started in port ${PORT}`))
